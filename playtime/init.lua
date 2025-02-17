b91b39c7233c0511bcdd12c618158f6e05661a57
Nanowolf4 <n4w@tutanota.com>
2025-02-17 09:16:22 +0200

    playtime: Add input validation to the /playtime and /regtime. Fix receiving of playtime for offline players

--- playtime/init.lua
+++ playtime/init.lua
@@ -5,6 +5,10 @@ local total = {}
 
 local storage = minetest.get_mod_storage()
 
+function playtime.load_playtime(name)
+	return storage:get_int("playtime:" .. name)
+end
+
 function playtime.get_current_playtime(name)
 	if not current[name] then
 		return 0
@@ -14,6 +18,10 @@ end
 
 -- Function to get playtime
 function playtime.get_total_playtime(name)
+	if not minetest.get_player_by_name(name) then
+		return playtime.load_playtime(name)
+	end
+
 	return total[name] + playtime.get_current_playtime(name)
 end
 
@@ -41,7 +49,7 @@ end)
 minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
 	current[name] = os.time()
-	total[name] = storage:get_int("playtime:" .. name)
+	total[name] = playtime.load_playtime(name)
 end)
 
 function playtime.seconds_to_clock(seconds)
@@ -59,40 +67,68 @@ end
 
 -- Registration time functions
 minetest.register_on_newplayer(function(ObjectRef)
-    local name = ObjectRef:get_player_name()
-    storage:set_int("regtime:" .. name, os.time())
+	local name = ObjectRef:get_player_name()
+	storage:set_int("regtime:" .. name, os.time())
 end)
 
 function playtime.get_registration_time(name)
 	local key = "regtime:" .. name
-    if storage:contains(key) then
-        return storage:get_int(key)
-    end
+	if storage:contains(key) then
+		return storage:get_int(key)
+	end
+	return 0
 end
 
 minetest.register_chatcommand("regtime", {
 	description = "Get registration time",
-	params = "<name>",
-    privs = {server = true},
+	params = "<playername>",
+	privs = {moderator = true},
 	func = function(name, param)
-        if param ~= "" then
-            local rtime = playtime.get_registration_time(param)
-            if rtime then
-                return true, os.date("%Y-%m-%d %H:%M:%S", rtime)
-            else
-                return true, "No entry"
-            end
-        else
-            return false, "No player name provided"
-        end
-    end
+		local target_name = param:trim()
+		if target_name == "" then
+			return false, "No player name provided"
+		end
+
+		if not minetest.player_exists(target_name) then
+			return false, "Player does not exist"
+		end
+
+		local rtime = playtime.get_registration_time(target_name)
+		if rtime == 0 then
+			return false, "No registration time found"
+		end
+
+		return true, os.date("%Y-%m-%d %H:%M:%S", rtime)
+	end
 })
 
 minetest.register_chatcommand("playtime", {
-	params = "",
-	description = "Use it to get your own playtime!",
-	func = function(name)
-		minetest.chat_send_player(name, "Total: " .. playtime.seconds_to_clock(playtime.get_total_playtime(name))
-			.. " Current: " .. playtime.seconds_to_clock(playtime.get_current_playtime(name)))
+	params = "<playername>",
+	description = "Shows total and current session playtime",
+	func = function(name, param)
+		local target_name = param:trim()
+		if target_name == "" then
+			target_name = name
+		elseif name ~= target_name and not minetest.check_player_privs(name, {moderator = true}) then
+			return false, "You do not have permission to see other players' playtime"
+		end
+
+		if not target_name then
+			return false, "Invalid player name"
+		end
+
+		if not minetest.player_exists(target_name) then
+			return false, "Player does not exist"
+		end
+
+		local ptime_total = playtime.get_total_playtime(target_name)
+		if ptime_total == 0 then
+			return false, "Player has no playtime"
+		end
+
+		local ptime_total_clock = playtime.seconds_to_clock(ptime_total)
+		local ptime_current_clock = playtime.seconds_to_clock(playtime.get_current_playtime(target_name))
+
+		return true, "Total playtime: " .. ptime_total_clock .. "\nCurrent playtime: " .. ptime_current_clock
 	end,
 })

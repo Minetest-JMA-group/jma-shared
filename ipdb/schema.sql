392ca08bb2803f6136fc07dd961c461058f6dc7c
Marko Petrović <petrovicmarko2006@gmail.com>
2026-02-12 21:52:05 +0100

    ipdb: Update schema

--- ipdb/schema.sql
+++ ipdb/schema.sql
@@ -32,6 +32,14 @@ CREATE TABLE Modstorage (
 	UNIQUE (userentry_id, modname, key)
 ) STRICT;
 
+CREATE TABLE Metadata (
+	key TEXT PRIMARY KEY,
+	value TEXT NOT NULL
+) STRICT;
+
+INSERT INTO Metadata (key, value) VALUES ('db_creation', CURRENT_TIMESTAMP);
+INSERT INTO Metadata (key, value) VALUES ('no_new_entries', 'false');
+
 CREATE INDEX idx_usernames_userentry ON Usernames(userentry_id);
 CREATE INDEX idx_ips_userentry ON IPs(userentry_id);
 CREATE INDEX idx_modstorage_user_mod_key ON Modstorage(userentry_id, modname, key);
